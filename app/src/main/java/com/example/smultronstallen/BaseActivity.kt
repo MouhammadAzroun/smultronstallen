0e557ccb9fedbb2de3dfcceef99c64f8317d12c2
mouhammadazroun <yhma22.azromo@folkuniversitetet.nu>
2023-01-01 18:48:32 +0100

    RecyclerViewAdapter

--- app/src/main/java/com/example/smultronstallen/BaseActivity.kt
+++ app/src/main/java/com/example/smultronstallen/BaseActivity.kt
@@ -2,10 +2,24 @@ package com.example.smultronstallen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class BaseActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecyclerViewAdapter(this, DataManager.placeList)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        recyclerView.adapter?.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/smultronstallen/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/smultronstallen/RecyclerViewAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.smultronstallen
 
-class RecyclerViewAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter(val context: Context, val places : List<Place>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
+    val layoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.list_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val place = places[position]
+        holder.heading.text = place.heading
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val heading = itemView.findViewById<TextView>(R.id.headingTextView)
+        var listItemPosition = 0
+
+    }
 }
